020f54b2c18ace6ab29ef628674486af5c1573e3
Soumen Das <dassoumen9832@gmail.com>
2023-01-18 22:12:22 +0530

    Create Swap_with3rd_var.py

---
+++ Swap_with3rd_var.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+print("Before Swap vale a =" + str(a) + " and b = " + str(b))
+temp = a
+a = b
+b = temp 
+print("After Swap vale a =" + str(a) + " and b = " + str(b))
+
