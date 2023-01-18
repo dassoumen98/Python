6470f442a61e25cdce6afd5fe0e4864fa426e014
Soumen Das <dassoumen9832@gmail.com>
2023-01-18 22:11:54 +0530

    Create Largest.py

---
+++ Largest.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the 1st Number:"))
+num2 = int(input("Enter the 2nd Number:"))
+if num1==num2:
+    print("Numbers is Equal")
+else:    
+    if num1>=num2:
+        print("Largest Number is:"+ str(num1))
+    else:
+         print("Largest Number is:"+ str(num2))  
