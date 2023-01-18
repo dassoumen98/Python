5a0b2298ed40bc061872838d3f05ccd752bd9752
Soumen Das <dassoumen9832@gmail.com>
2023-01-18 22:12:08 +0530

    Create Simple_Interest.py

---
+++ Simple_Interest.py
@@ -0,0 +1,27 @@
+a=int(input("Enter the Principle Amount:"))
+b=int(input("Enter the Time in Years:"))
+c=int(input("Enter the Annual Rate of Interset:"))
+def simple_interset(p,n,r):
+   
+    si=(p*n*r)/100
+    print("Simple Interest is:" +str(si))
+
+simple_interset(a , b , c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
