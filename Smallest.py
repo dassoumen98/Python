958478b69c3cd35dd1156b3408c2e5f564b974b9
Soumen Das <dassoumen9832@gmail.com>
2023-01-18 22:12:11 +0530

    Create Smallest.py

---
+++ Smallest.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the 1st Number:"))
+num2 = int(input("Enter the 2nd Number:"))
+if num1<num2:
+    print("smallest Number is:",(num1))
+else:
+     print("smallest Number is:",(num2))  
