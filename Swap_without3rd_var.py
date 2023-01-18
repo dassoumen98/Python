291643bad5ac16cf62e5809d012ccacbdcf1c9e3
Soumen Das <dassoumen9832@gmail.com>
2023-01-18 22:12:25 +0530

    Create Swap_without3rd_var.py

---
+++ Swap_without3rd_var.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+print("Before Swap vale a =" + str(a) + " and b = " + str(b))
+a = a+b 
+b = a-b 
+a = a-b 
+print("After Swap vale a =" + str(a) + " and b = " + str(b))
